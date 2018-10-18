6a95b77283afb0962c45825374c4e14bca2ef797
junlee91 <jlee757@uic.edu>
2018-10-17 20:05:28 -0500

    ToggleDrivingMode

---
+++ src/api/User/ToggleDrivingMode/ToggleDrivingMode.resolvers.ts
@@ -0,0 +1,22 @@
+import User from "../../../entities/User";
+import { ToggleDrivingModeResoponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    ToggleDrivingMode: privateResolver(
+      async (_, __, { req }): Promise<ToggleDrivingModeResoponse> => {
+        const user: User = req.user;
+        user.isDriving = !user.isDriving;
+        user.save();
+        return {
+          ok: true,
+          error: null
+        };
+      }
+    )
+  }
+};
+
+export default resolvers;
